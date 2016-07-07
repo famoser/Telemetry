702838fdfbed2ede17ca6e9df98d02aadf59f673
Florian Moser <git@famoser.ch>
2016-07-07 19:13:27 +0200

    initial commit

---
+++ Famoser.Telemetry.UniversalWindows/FamoserTelemetry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Famoser.FrameworkEssentials.Logging;
+using Famoser.Telemetry.UniversalWindows.Logger;
+
+namespace Famoser.Telemetry.UniversalWindows
+{
+    public class FamoserTelemetry
+    {
+        private static string _url = "https://telemetry.famoser.ch";
+
+        public static void Initialize(string applicationId)
+        {
+            LogHelper.Instance.OverwriteLogger(new TelemetryLogger());
+        }
+
+        public static void ConfigureUrl(string url)
+        {
+            _url = url;
+        }
+    }
+}
---
+++ Famoser.Telemetry.UniversalWindows/Logger/TelemetryLogger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Famoser.FrameworkEssentials.Logging;
+using Famoser.FrameworkEssentials.Logging.Interfaces;
+
+namespace Famoser.Telemetry.UniversalWindows.Logger
+{
+    public class TelemetryLogger : ILogger
+    {
+        public TelemetryLogger(string url, string applicationId)
+        {
+            
+        }
+
+        public void AddLog(LogModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ClearLogs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<LogModel> GetLogs(bool clearAfterwards = true)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
