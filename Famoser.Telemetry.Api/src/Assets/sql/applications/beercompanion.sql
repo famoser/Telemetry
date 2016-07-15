2b1209de11afbf628a6c83bad39dd277583573aa
Florian Moser <git@famoser.ch>
2016-07-15 17:18:12 +0200

    added license

---
+++ Famoser.Telemetry.Api/src/Assets/sql/applications/beercompanion.sql
@@ -0,0 +1,3 @@
+INSERT INTO applications (id, guid, name, description, projectUrl) VALUES
+  (7, '5718d6f1-9005-4aa3-b64c-032878b9e4d5', 'Beer Companion',
+   'a simple beer counter, with api sync', 'https://github.com/famoser/BeerCompanion')
---
+++ Famoser.Telemetry.Api/src/Assets/sql/applications/expensemonitor.sql
@@ -0,0 +1,3 @@
+INSERT INTO applications (id, guid, name, description, projectUrl) VALUES
+  (6, '2c8d148e-6800-49b5-af10-39905d32ea5c', 'Expense Monitor',
+   'keep track of your expenses', 'https://github.com/famoser/ExpenseMonitor')
